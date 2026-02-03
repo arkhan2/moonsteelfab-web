3de82b04651451aac1c92e1d4c9d3249495ef659
arkhan2 <75585941+arkhan2@users.noreply.github.com>
2026-02-03 13:48:16 +0500

    Update env.ts

--- web/src/lib/env.ts
+++ web/src/lib/env.ts
@@ -1,5 +1,5 @@
 export const env = {
   apiBaseUrl:
-    process.env.NEXT_PUBLIC_API_BASE_URL?.replace(/\/+$/, "") ?? "http://127.0.0.1:8787"
+    process.env.NEXT_PUBLIC_API_BASE_URL?.replace(/\/+$/, "https://moonsteelfab-api.mynickar.workers.dev") ?? ""
 };
 
